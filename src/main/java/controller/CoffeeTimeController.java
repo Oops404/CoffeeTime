53a58147dc7ee5340f3308114af1da1332cb6c2a
Oops404 <cheneyjin@outlook.com>
2019-04-17 19:53:43 +0800

    add javafx, remove useless.

--- src/main/java/controller/CoffeeTimeController.java
+++ src/main/java/controller/CoffeeTimeController.java
@@ -2,10 +2,16 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +22,8 @@ import java.util.ResourceBundle;
  * @Email cheneyjin@outlook.com
  */
 public class CoffeeTimeController implements Initializable {
+    @FXML
+    private BorderPane borderPane;
     @FXML
     private Button loadConfigButton;
     @FXML
@@ -30,6 +38,20 @@ public class CoffeeTimeController implements Initializable {
 
     public void loadConfigButtonOnClicked() {
         System.out.println("load");
+        String workingFolder = System.getProperty("user.dir");
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extensionFilter
+                = new FileChooser.ExtensionFilter("ini file (*.ini)", "*.ini");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+        fileChooser.setInitialDirectory(new File(workingFolder));
+
+        Scene scene = this.borderPane.getScene();
+        Window window = scene.getWindow();
+        Stage stage = (Stage) window;
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            System.out.println(file.length());
+        }
     }
 
     public void saveConfigButtonOnClicked() {
